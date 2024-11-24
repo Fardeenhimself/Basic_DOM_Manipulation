c75dcd6bd929e3ef9958cea57cb8c1f1eca64ef4
Fardeenhimself <fardeenviper77@gmail.com>
2024-11-24 16:28:16 +0600

    Fixed a bug of empty string

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const list = document.querySelector("#movie-list ul");
   const form = document.forms;
 
-  //delete movie
+  // Delete movie
   list.addEventListener("click", (e) => {
     if (e.target.className === "delete") {
       const li = e.target.parentElement;
@@ -11,33 +11,41 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  //add movie
+  // Add movie
   const addForm = form["add-movie"];
   addForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    //create elements for dynamic adding
-    const value = addForm.querySelector('input[type="text"]').value;
+    // Get the input value
+    const value = addForm.querySelector('input[type="text"]').value.trim();
+
+    // Checking empty string!
+    if (value === "") {
+      alert("Please enter a movie name.");
+      return;
+    }
+
+    // Create elements for dynamic list
     const li = document.createElement("li");
     const movieName = document.createElement("span");
     const deleteBtn = document.createElement("span");
     const visitBtn = document.createElement("span");
 
-    //text content
+    // Adding Text content
     movieName.textContent = value;
     deleteBtn.textContent = "Delete";
     visitBtn.textContent = "Visit";
 
-    //adding css class
+    // Add CSS classes
     movieName.classList.add("name");
     deleteBtn.classList.add("delete");
     visitBtn.classList.add("visit");
 
-    //finally append to list
+    // Finally append to the list
     li.appendChild(movieName);
     li.appendChild(deleteBtn);
     li.appendChild(visitBtn);
-    list.appendChild(li); //bug(1):- li didn't work
+    list.appendChild(li); // bug(1): li was not working!
     alert("Added Successfully!!");
   });
 });
